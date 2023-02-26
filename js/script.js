6b331f81398c1a6cfe41bf17963b4208186f3e71
adam <adambers2020@gmail.com>
2023-02-27 01:22:18 +0300

    changed event name1221

--- js/script.js
+++ js/script.js
@@ -14,6 +14,7 @@ document.getElementById("range").addEventListener("change", function (e) {
     console.log("click")
     // e.preventDefault();
     // e.stopPropagation();
+    alert("hello")
 
     if (e.target.value > 0) {
         document.getElementById("initial-range").style.background = "#ED712E"
