0c525711d25d27f209fcff6c109185afff79c201
adam <adambers2020@gmail.com>
2023-02-27 11:35:27 +0300

    dsfsdfs

--- js/script.js
+++ js/script.js
@@ -82,7 +82,7 @@ function rangeHandler(e) {
 }
 
 
-document.getElementById("range").addEventListener("input", (e) => rangeHandler(e))
+document.getElementById("range").addEventListener("change", (e) => rangeHandler(e))
 // document.getElementById("range").addEventListener("touchstart", (e) => rangeHandler(e))
 // document.getElementById("range").addEventListener("touchmove", (e) => rangeHandler(e))
 // document.getElementById("range").addEventListener("touchend", (e) => rangeHandler(e))
