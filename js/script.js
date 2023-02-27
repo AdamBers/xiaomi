e1cafea2f428c3e60f5bad4880356d22b2916da9
adam <adambers2020@gmail.com>
2023-02-27 11:54:04 +0300

    dsfsdfs

--- js/script.js
+++ js/script.js
@@ -83,7 +83,7 @@ function rangeHandler(e) {
 
 
 document.getElementById("range").addEventListener("change", (e) => rangeHandler(e))
-document.getElementById("range").addEventListener("touchstart", (e) => rangeHandler(e))
+// document.getElementById("range").addEventListener("touchstart", (e) => rangeHandler(e))
 document.getElementById("range").addEventListener("touchmove", (e) => rangeHandler(e))
 document.getElementById("range").addEventListener("touchend", (e) => rangeHandler(e))
 
